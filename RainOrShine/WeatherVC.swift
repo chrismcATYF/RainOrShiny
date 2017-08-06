03fb9477ffc03336174faf9d3ce0b0295a756b7a
Chris McDonald <chrismcdonald@Chriss-MacBook-Air.local>
2017-08-06 03:18:01 -0500

    setup completed

---
+++ RainOrShine/WeatherVC.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherVC.swift
+//  RainOrShine
+//
+//  Created by Chris McDonald on 8/6/17.
+//  Copyright © 2017 Chris McDonald. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+}
+
