2e00e7929ff74a9bfe78174c395bf00856725ad0
Chris McDonald <chrismcdonald@Chriss-MacBook-Air.local>
2017-08-06 16:17:43 -0500

    Added table view

--- RainOrShine/WeatherVC.swift
+++ RainOrShine/WeatherVC.swift
@@ -8,14 +8,36 @@
 
 import UIKit
 
-class WeatherVC: UIViewController {
-
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var dateLbl: UILabel!
+    @IBOutlet weak var tempLbl: UILabel!
+    @IBOutlet weak var locationLbl: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var weatherTypeLbl: UILabel!
+    @IBOutlet weak var weatherTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        weatherTableView.delegate = self
+        weatherTableView.dataSource = self
+        
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        
+        return cell
+    }
     
 }
 
