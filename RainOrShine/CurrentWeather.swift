229a3e71c2ef7013758e7a62a7b75692fefa8a49
Chris McDonald <chrismcdonald@Chriss-MacBook-Air.local>
2017-08-09 00:29:45 -0500

    Clean up

--- RainOrShine/CurrentWeather.swift
+++ RainOrShine/CurrentWeather.swift
@@ -14,14 +14,12 @@ class CurrentWeather {
     var _date: String!
     var _weatherType: String!
     var _currentTemp: Double!
-    
     var cityName: String {
         if _cityName == nil {
             _cityName = ""
         }
         return _cityName
     }
-    
     var date: String {
         if _date == nil {
             _date = ""
@@ -33,14 +31,12 @@ class CurrentWeather {
         self._date = "Today, \(currentDate)"
         return _date
     }
-    
     var weatherType: String {
         if _weatherType == nil {
             _weatherType = ""
         }
         return _weatherType
     }
-    
     var currentTemp: Double {
         if _currentTemp == nil {
             _currentTemp = 0.0
@@ -56,14 +52,12 @@ class CurrentWeather {
                     self._cityName = name.capitalized
                     print(self._cityName)
                 }
-                
                 if let weather = dict["weather"] as? [Dictionary<String, AnyObject>] {
                     if let main = weather[0]["main"] as? String {
                         self._weatherType = main.capitalized
                         print(self._weatherType)
                     }
                 }
-                
                 if let main = dict["main"] as? Dictionary<String, AnyObject> {
                     if let currentTemp = main["temp"] as? Double {
                         let kelvinToFarenheitPre = (currentTemp * (9 / 5) - 459.67)
